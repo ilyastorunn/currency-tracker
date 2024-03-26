f620173a4238714233b733c7d245098281d24333
İlyas Torun <ilyastorun@Ilyas-MacBook-Air.local>
2024-03-26 12:04:33 +0300

    made home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,27 +10,55 @@ import cryptoAI from "../pics/cryptoAI.jpeg";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 grid-rows-12 gap-6 m-6">
-      <div className="col-span-4 row-span-6 border-2 rounded-[32px] border-indigo-500/50">
-        <img className="w-[448px] h-[476px] rounded-[32px]" src={stockMarket} />
+    <div className="grid gap-4 grid-cols-12 m-2 h-screen">
+      <div className="col-span-4 row-span-3 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+          <img className="w-full h-full object-cover" src={stockMarket} />
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-[32px]">
+            <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent">
+              take a look at <br /> stock market
+            </p>
+          </div>
       </div>
-      <div className="col-span-5 row-span-4 border-2 rounded-3xl border-indigo-500/50">
-        <img className="w-[566px] h-[309px] rounded-[32px]" src={currencies} />
+      <div className="bg-red-500 col-span-5 row-span-2 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={currencies} />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-[32px]">
+          <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent text-center">
+            track currencies <br /> in real time
+          </p>
+        </div>
       </div>
-      <div className="col-span-2 row-span-8 border-2 rounded-3xl border-indigo-500/50">
-        <img class="w-[330px] h-[643px] rounded-[32px]" src={coinList} />
+      <div className="bg-red-500 col-span-3 row-span-4 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={coinList} />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-[32px]">
+          <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent text-center">
+            follow your <br /> crypto <br /> currencies
+          </p>
+        </div>
       </div>
-      <div className="col-span-2 row-span-4 border-2 rounded-3xl border-indigo-500/50">
-        <img class="w-[212px] h-[309px] rounded-[32px]" src={graph} />
+      <div className="bg-red-500 col-span-2 row-span-2 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={graph} />
       </div>
-      <div className="col-span-3 row-span-4 border-2 rounded-3xl border-indigo-500/50">
-        <img class="w-[330px] h-[309px] rounded-[32px]" src={cryptoAI} />
+      <div className="bg-red-500 col-span-3 row-span-2 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={cryptoAI} />
       </div>
-      <div className="col-span-4 row-span-6 border-2 rounded-3xl border-indigo-500/50">
-        <img class="w-[448px] h-[476px] rounded-[32px]" src={crypto} />
+      <div className="bg-red-500 col-span-4 row-span-3 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={crypto} />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-[32px] flex-col">
+          <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent mb-20 text-center">
+            your <br /> favorite <br /> currencies
+          </p>
+          <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent">
+            coming soon!
+          </p>
+        </div>
       </div>
-      <div className="col-span-8 border-2 rounded-3xl border-indigo-500/50">
-        
+      <div className="bg-red-500 col-span-8 row-span-2 rounded-[32px] flex items-center justify-center overflow-hidden relative">
+        <img className="w-full h-full object-cover" src={compare} />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-[32px]">
+          <p className="text-white text-[40px] font-medium font-['Oswald'] uppercase bg-transparent">
+            compare your currencies
+          </p>
+        </div>
       </div>
     </div>
   );
